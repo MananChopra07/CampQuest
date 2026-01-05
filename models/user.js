326a70df6aeef1fbd3aea7256bfd57f98c1ad336
Manan Chopra <mananchopra@Manans-MacBook-Pro.local>
2026-01-05 20:49:43 +0530

    Fix passport-local-mongoose casing for Linux

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
-let passportLocalMongoose = require('passport-Local-Mongoose');
+let passportLocalMongoose = require('passport-local-mongoose');
+
 let UserSchema = new Schema({
     email:{
         type : String,
